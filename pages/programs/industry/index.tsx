bd839ea5369beef79747f8f863e889f4e12977bb
Novailoveyou <novailoveyoux@gmail.com>
2021-03-22 17:56:02 +0300

    changed page structure a bit

---
+++ pages/programs/industry/index.tsx
@@ -0,0 +1,7 @@
+import Programs from '../../../components/pages/Programs'
+
+const industry = () => {
+  return <Programs />
+}
+
+export default industry
---
+++ pages/programs/mini/index.tsx
@@ -0,0 +1,7 @@
+import Programs from '../../../components/pages/Programs'
+
+const mini = () => {
+  return <Programs />
+}
+
+export default mini
