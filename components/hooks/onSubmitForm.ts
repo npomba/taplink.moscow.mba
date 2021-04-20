4894fd7d31874e40a3214fa5b3c6093f7deb9942
Novailoveyou <novailoveyoux@gmail.com>
2021-04-20 21:22:40 +0300

    made email work

--- components/hooks/onSubmitForm.ts
+++ components/hooks/onSubmitForm.ts
@@ -5,7 +5,7 @@ const onSubmitForm = async (values) => {
   try {
     const res = await axios.post(`${thisServer}/api/contact`, values)
     res.status === 200 && console.log('Success')
-    res.status === 500 && console.log('Error')
+    // res.status === 500 && console.log('Error')
   } catch (err) {
     console.log(err)
   }
--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -7,20 +7,34 @@ export default async (req, res) => {
 
   // const testAccount = await nodemailer.createTestAccount()
 
+  //   SMTP_HOST=smtp.jino.ru
+  // SMTP_LOGIN=temp@testmba.ipo.msk.ru
+  // SMTP_PASS=YGgaaz9ruAzKz6Y
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
+    // host: 'smtp.jino.ru',
+    // host: 'smtp.ethereal.email',
     port: 587,
     secure: false, // true for 465, false for other ports
+    logger: true,
+    debug: true,
+    tls: {
+      rejectUnAuthorized: true,
+    },
     auth: {
       user: process.env.SMTP_LOGIN,
       pass: process.env.SMTP_PASS,
+      // user: 'temp@testmba.ipo.msk.ru',
+      // pass: 'YGgaaz9ruAzKz6Y',
+      // user: testAccount.user,
+      // pass: testAccount.pass,
     },
   })
 
   try {
     const emailRes = await transporter.sendMail({
       from: 'temp@testmba.ipo.msk.ru',
-      to: 'nova@ipo.msk.ru, novailoveyou3@gmail.com', // list of receivers
+      to: 'nova@ipo.msk.ru, novailoveyou3@gmail.com, marketing@rosucheba.ru', // list of receivers
       subject: 'Новая заявка с moscow.mba', // Subject line
       text: `${name}, ${phone}`, // plain text body
       html: `${name}, ${phone}`, // html body
@@ -29,8 +43,8 @@ export default async (req, res) => {
     console.log('Message sent: %s', emailRes.messageId)
     res.status(200).json({ status: 200, msg: 'Email is sent' })
   } catch (err) {
-    res.status(500).json({ status: 500, msg: 'Unexpected server error' })
+    res.status(500).json({ status: 500, err, msg: 'Unexpected server error' })
 
-    console.log(err)
+    console.error(err)
   }
 }
