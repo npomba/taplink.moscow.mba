acfe1e5c02d0240821ecd6f2d9baa7a7e175fe6a
Novailoveyou <novailoveyoux@gmail.com>
2021-04-20 21:34:35 +0300

    send to different emails depending on env

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -1,12 +1,14 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 import nodemailer from 'nodemailer'
+import { thisServer } from '@/config/index'
 
 export default async (req, res) => {
   const { name, phone } = req.body
 
   // const testAccount = await nodemailer.createTestAccount()
 
+  console.log(thisServer)
   const transporter = nodemailer.createTransport({
     host: process.env.SMTP_HOST,
     port: 587,
@@ -25,7 +27,11 @@ export default async (req, res) => {
   try {
     const emailRes = await transporter.sendMail({
       from: 'temp@testmba.ipo.msk.ru',
-      to: 'nova@ipo.msk.ru, novailoveyou3@gmail.com, marketing@rosucheba.ru', // list of receivers
+      to: `${
+        thisServer === 'http://localhost:3000'
+          ? 'nova@ipo.msk.ru, novailoveyou3@gmail.com'
+          : 'marketing@rosucheba.ru'
+      }`,
       subject: 'Новая заявка с moscow.mba', // Subject line
       text: `${name}, ${phone}`, // plain text body
       html: `${name}, ${phone}`, // html body
