225453320df4e76c4055ce7881abc708400008d6
novailoveyou <novailoveyoux@gmail.com>
2021-04-24 12:05:52 +0300

    replaced img with Image in Diploma

--- components/sections/Diploma.tsx
+++ components/sections/Diploma.tsx
@@ -1,11 +1,15 @@
+import Image from 'next/image'
+
 const Diploma = () => {
 	return (
 		<section className='diplom-section'>
 			<div className='image'>
-				<img
-					className='lazyload'
-					data-src='/assets/images/diploma.jpg'
-					alt=''
+				<Image
+					src='/assets/images/diploma.jpg'
+					alt='Ваш будущий диплом'
+					width={532}
+					height={376}
+					layout={'responsive'}
 				/>
 			</div>
 			<div className='content'>
