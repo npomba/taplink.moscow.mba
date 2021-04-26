abbd876208650124e4457462f1e5465be991f39b
Novailoveyou <novailoveyoux@gmail.com>
2021-04-26 15:13:36 +0300

    chagned fallbacks to false

--- components/pages/BlendedProgram.tsx
+++ components/pages/BlendedProgram.tsx
@@ -23,55 +23,56 @@ import CostOfStudy from '@/components/sections/CostOfStudy'
 // import loadJs from 'loadjs'
 
 const PageBlendedProgram = ({ program }) => {
-	// useEffect(() => {
+  // useEffect(() => {
 
-	//   loadJs([
-	//     '/assets/js/slick.min.js',
-	//     '/assets/js/lazysizes.min.js',
-	//     '/assets/js/circle-progress.min.js',
-	//     '/assets/js/jquery.magnific-popup.min.js',
-	//     '/assets/js/myjs.js'
-	//   ], {
-	//     async: false
-	//   })
-	// }, [])
-	const data = program
+  //   loadJs([
+  //     '/assets/js/slick.min.js',
+  //     '/assets/js/lazysizes.min.js',
+  //     '/assets/js/circle-progress.min.js',
+  //     '/assets/js/jquery.magnific-popup.min.js',
+  //     '/assets/js/myjs.js'
+  //   ], {
+  //     async: false
+  //   })
+  // }, [])
+  const data = program
+  console.log(data)
 
-	return (
-		<>
-			<Head>
-				<title>{data.title}</title>
-			</Head>
+  return (
+    <>
+      <Head>
+        <title>{data.title}</title>
+      </Head>
 
-			<JumbotronProgram data={data} format={'blended'} />
+      <JumbotronProgram data={data} format={'blended'} />
 
-			<div className='container'>
-				<ProgramGoal data={data} />
-				<WhatWillYouLearn data={data} />
-				<ProgramDesc />
-				<BlendedMetups />
-				<Pros format={'blended'} />
-				<HowProcessGoes />
-				<ProgramsModules />
-				<ContactUs title={'Получите консультацию по программе'} />
-				<Teachers />
-				<RelevantContent />
-				<CorporateClients />
-				<Accreditation />
-				<Rules prices={{ lowerPrice: '300 000', higherPrice: '1 500 000' }} />
-				<Diploma />
-				<Students />
-				<Reviews />
-				<CostOfStudy />
-				<Faq />
-				<ContactUs
-					title={
-						'Не знаете что выбрать? Получите консультацию по программам MBA'
-					}
-				/>
-			</div>
-		</>
-	)
+      <div className='container'>
+        <ProgramGoal data={data} />
+        <WhatWillYouLearn data={data} />
+        <ProgramDesc />
+        <BlendedMetups />
+        <Pros format={'blended'} />
+        <HowProcessGoes />
+        <ProgramsModules />
+        <ContactUs title={'Получите консультацию по программе'} />
+        <Teachers />
+        <RelevantContent />
+        <CorporateClients />
+        <Accreditation />
+        <Rules prices={{ lowerPrice: '300 000', higherPrice: '1 500 000' }} />
+        <Diploma />
+        <Students />
+        <Reviews />
+        <CostOfStudy />
+        <Faq />
+        <ContactUs
+          title={
+            'Не знаете что выбрать? Получите консультацию по программам MBA'
+          }
+        />
+      </div>
+    </>
+  )
 }
 
 export default PageBlendedProgram
--- pages/programs/professional/blended/[url].tsx
+++ pages/programs/professional/blended/[url].tsx
@@ -3,56 +3,56 @@ import { server } from '@/config/index'
 import BlendedProgram from '@/components/pages/BlendedProgram'
 
 const programsProfessionalBlendedProgram = ({ program }) => {
-	return <BlendedProgram program={program} />
+  return <BlendedProgram program={program} />
 }
 
-export const getStaticProps = async context => {
-	const res = await fetch(
-		`${server}/api/v1/bootcamps/605c5f71bc557b46b4f42a56/courses`
-	)
-	const { data } = await res.json()
+export const getStaticProps = async (context) => {
+  const res = await fetch(
+    `${server}/api/v1/bootcamps/605c5f71bc557b46b4f42a56/courses`
+  )
+  const { data } = await res.json()
 
-	const programs = data.filter(
-		item =>
-			item.url === context.params.url &&
-			item.mbaFormat === 'blended' &&
-			item.mbaTypeOfProgram === 'professional'
-	)
+  const programs = data.filter(
+    (item) =>
+      item.url === context.params.url &&
+      item.mbaFormat === 'blended' &&
+      item.mbaTypeOfProgram === 'professional'
+  )
 
-	const program = programs[0]
+  const program = programs[0]
 
-	return {
-		props: {
-			program,
-		},
-	}
+  return {
+    props: {
+      program,
+    },
+  }
 }
 
 export const getStaticPaths = async () => {
-	const res = await fetch(
-		`${server}/api/v1/bootcamps/605c5f71bc557b46b4f42a56/courses`
-	)
-	const programs = await res.json()
+  const res = await fetch(
+    `${server}/api/v1/bootcamps/605c5f71bc557b46b4f42a56/courses`
+  )
+  const programs = await res.json()
 
-	const urls = programs.data
-		.map(program => {
-			if (
-				program.mbaFormat === 'blended' &&
-				program.mbaTypeOfProgram === 'professional'
-			) {
-				return { id: program._id, url: program.url && program.url }
-			}
-		})
-		.filter(program => program !== undefined)
+  const urls = programs.data
+    .map((program) => {
+      if (
+        program.mbaFormat === 'blended' &&
+        program.mbaTypeOfProgram === 'professional'
+      ) {
+        return { id: program._id, url: program.url && program.url }
+      }
+    })
+    .filter((program) => program !== undefined)
 
-	const paths = urls.map(item => ({
-		params: { url: item.url.toString() },
-	}))
+  const paths = urls.map((item) => ({
+    params: { url: item.url.toString() },
+  }))
 
-	return {
-		paths,
-		fallback: true,
-	}
+  return {
+    paths,
+    fallback: false,
+  }
 }
 
 export default programsProfessionalBlendedProgram
