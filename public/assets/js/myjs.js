25b97d06932c8286010a9bb57403582bc52de3ef
Novailoveyou <novailoveyoux@gmail.com>
2021-04-26 18:44:53 +0300

    added stuck overflow link to the piece of code

--- public/assets/js/myjs.js
+++ public/assets/js/myjs.js
@@ -176,6 +176,7 @@ if ($('.mini-programs-slider').length) {
 }
 
 // Hide some jQuery slgs
+// https://stackoverflow.com/questions/39152877/consider-marking-event-handler-as-passive-to-make-the-page-more-responsive
 jQuery.event.special.touchstart = {
   setup: function( _, ns, handle ) {
       this.addEventListener('touchstart', handle, { passive: !ns.includes('noPreventDefault') });
