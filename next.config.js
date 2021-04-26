4eb8f6f0ff995a7ce3facaf0278f09e0c2fcd582
Novailoveyou <novailoveyoux@gmail.com>
2021-04-26 20:25:02 +0300

    added secure headers

--- next.config.js
+++ next.config.js
@@ -1,7 +1,54 @@
+const { createSecureHeaders } = require('next-secure-headers')
+
 module.exports = {
-	i18n: {
-		locales: ['ru', 'en-US'],
-		defaultLocale: 'ru',
-		localeDetection: false,
-	},
+  i18n: {
+    locales: ['ru', 'en-US'],
+    defaultLocale: 'ru',
+    localeDetection: false,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: createSecureHeaders({
+          contentSecurityPolicy: {
+            directives: {
+              defaultSrc: "'self'",
+              styleSrc: ["'self'", 'https://stackpath.bootstrapcdn.com'],
+            },
+          },
+          forceHTTPSRedirect: [
+            true,
+            { maxAge: 60 * 60 * 24 * 4, includeSubDomains: true },
+          ],
+          referrerPolicy: 'same-origin',
+        }),
+      },
+    ]
+  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       headers: [
+  //         {
+  //           key: 'X-XSS-Protection',
+  //           value: '1',
+  //         },
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: "default-src 'self' ipo-cp.ru *.ipo-cp.ru",
+  //         },
+  //         {
+  //           key: 'X-Frame-Options',
+  //           value: 'DENY',
+  //         },
+  //         {
+  //           key: 'X-Content-Type-Options',
+  //           value: 'nosniff',
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
 }
