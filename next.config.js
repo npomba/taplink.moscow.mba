6b21b90f73cf8739d3dfd8faf0964bc09e7bd510
Novailoveyou <novailoveyoux@gmail.com>
2021-04-27 14:33:24 +0300

    allowed vercel contentSecurityPolicy

--- next.config.js
+++ next.config.js
@@ -1,61 +1,65 @@
 const { createSecureHeaders } = require('next-secure-headers')
 
 module.exports = {
-	poweredByHeader: false,
-	i18n: {
-		locales: ['ru', 'en-US'],
-		defaultLocale: 'ru',
-		localeDetection: false,
-	},
-	async headers() {
-		return [
-			{
-				source: '/(.*)',
-				headers: createSecureHeaders({
-					forceHTTPSRedirect: [
-						true,
-						{ maxAge: 60 * 60 * 24 * 30 * 12 * 2, includeSubDomains: true },
-					],
-					frameGuard: 'deny',
-					noopen: 'noopen',
-					nosniff: 'nosniff',
-					xssProtection: 'sanitize',
-					contentSecurityPolicy: {
-						directives: {
-							connectSrc: ["'self'", 'https://ipo-cp.ru'],
-							defaultSrc: ["'self'", 'https://ipo-cp.ru'],
-							scriptSrc: ["'self'"],
-							styleSrc: ["'self'"],
-						},
-					},
-					referrerPolicy: 'no-referrer',
-				}),
-			},
-		]
-	},
-	// async headers() {
-	//   return [
-	//     {
-	//       source: '/',
-	//       headers: [
-	//         {
-	//           key: 'X-XSS-Protection',
-	//           value: '1',
-	//         },
-	//         {
-	//           key: 'Content-Security-Policy',
-	//           value: "default-src 'self' ipo-cp.ru *.ipo-cp.ru",
-	//         },
-	//         {
-	//           key: 'X-Frame-Options',
-	//           value: 'DENY',
-	//         },
-	//         {
-	//           key: 'X-Content-Type-Options',
-	//           value: 'nosniff',
-	//         },
-	//       ],
-	//     },
-	//   ]
-	// },
+  poweredByHeader: false,
+  i18n: {
+    locales: ['ru', 'en-US'],
+    defaultLocale: 'ru',
+    localeDetection: false,
+  },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: createSecureHeaders({
+          forceHTTPSRedirect: [
+            true,
+            { maxAge: 60 * 60 * 24 * 30 * 12 * 2, includeSubDomains: true },
+          ],
+          frameGuard: 'deny',
+          noopen: 'noopen',
+          nosniff: 'nosniff',
+          xssProtection: 'sanitize',
+          contentSecurityPolicy: {
+            directives: {
+              connectSrc: [
+                "'self'",
+                'https://ipo-cp.ru',
+                'vitals.vercel-insights.com',
+              ],
+              defaultSrc: ["'self'", 'https://ipo-cp.ru'],
+              scriptSrc: ["'self'"],
+              styleSrc: ["'self'"],
+            },
+          },
+          referrerPolicy: 'no-referrer',
+        }),
+      },
+    ]
+  },
+  // async headers() {
+  //   return [
+  //     {
+  //       source: '/',
+  //       headers: [
+  //         {
+  //           key: 'X-XSS-Protection',
+  //           value: '1',
+  //         },
+  //         {
+  //           key: 'Content-Security-Policy',
+  //           value: "default-src 'self' ipo-cp.ru *.ipo-cp.ru",
+  //         },
+  //         {
+  //           key: 'X-Frame-Options',
+  //           value: 'DENY',
+  //         },
+  //         {
+  //           key: 'X-Content-Type-Options',
+  //           value: 'nosniff',
+  //         },
+  //       ],
+  //     },
+  //   ]
+  // },
 }
