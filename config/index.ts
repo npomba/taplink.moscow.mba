beecfa61fec08acb484abbae0d4c4c3d0eb56756
Novailoveyou <novailoveyoux@gmail.com>
2021-04-30 20:55:39 +0300

    commit b4 leaving the office

--- config/index.ts
+++ config/index.ts
@@ -1,6 +1,6 @@
 const dev = process.env.NODE_ENV !== 'production'
 
-const server = dev ? 'http://localhost:5000' : 'https://ipo-cp.ru'
+const server = dev ? 'http://10.0.0.151:5000' : 'https://ipo-cp.ru'
 
 const thisServer = dev ? 'http://localhost:3000' : 'https://moscow.mba'
 
