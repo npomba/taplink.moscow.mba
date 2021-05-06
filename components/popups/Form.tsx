ab8fdcb0ca549730257270b8e7376b87132c7eb2
novailoveyou <novailoveyoux@gmail.com>
2021-05-06 12:44:33 +0300

    testing dataLayer for ym ecommerce

--- components/popups/Form.tsx
+++ components/popups/Form.tsx
@@ -11,133 +11,136 @@ import { useEffect } from 'react'
 import loadJs from 'loadjs'
 
 type FormValues = {
-  name: string
-  phone: string
+	name: string
+	phone: string
 }
 
 const Form = ({
-  closePopUp,
-  programTitle = null,
-  programId = null,
-  title = setString(lang.helpToChooseTitle),
-  disc = setString(lang.helpToChooseDics),
+	closePopUp,
+	programTitle = null,
+	programId = null,
+	title = setString(lang.helpToChooseTitle),
+	disc = setString(lang.helpToChooseDics),
 }) => {
-  const at = useAt()
+	const at = useAt()
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormValues>()
 
-  useEffect(() => {
-    loadJs(['/assets/js/formPlaceholder.js'], {
-      async: false,
-    })
+	useEffect(() => {
+		loadJs(['/assets/js/formPlaceholder.js'], {
+			async: false,
+		})
 
-    const tagManagerArgs = {
-      dataLayer: {
-        event: 'generate_lead',
-        ecommerce: {
-          items: [
-            {
-              id: programId,
-              name: programTitle,
-              programFormat: at.online
-                ? 'online'
-                : at.blended
-                ? 'blended'
-                : null,
-              programType: at.mini
-                ? 'mini'
-                : at.professional
-                ? 'professional'
-                : at.industry
-                ? 'industry'
-                : null,
-            },
-          ],
-        },
-      },
-      dataLayerName: 'dataLayer',
-    }
-    TagManager.dataLayer(tagManagerArgs)
-  }, [])
+		const tagManagerArgs = {
+			dataLayer: {
+				event: 'generate_lead',
+				ecommerce: {
+					detail: {
+						actionField: {
+							id: programId,
+						},
+						products: [
+							{
+								id: programId,
+								name: programTitle,
+								programFormat: at.online
+									? 'online'
+									: at.blended
+									? 'blended'
+									: null,
+								programType: at.mini
+									? 'mini'
+									: at.professional
+									? 'professional'
+									: at.industry
+									? 'industry'
+									: null,
+							},
+						],
+					},
+				},
+			},
+			dataLayerName: 'dataLayer',
+		}
+		TagManager.dataLayer(tagManagerArgs)
+	}, [])
 
-  return (
-    <div id='teachersModal' className='popup-modal mfp-hide mfp-with-anim'>
-      <div className='popup-content-origin red-bg'>
-        <h2>{title}</h2>
-        <div className='desc'>
-          {!programTitle && disc}{' '}
-          {programTitle &&
-            `Оставьте заявку и получите консультацию по программе ${programTitle}, узнайте возможные варианты скидок и требования к поступлению`}
-        </div>
-        <form
-          method='post'
-          className='simple-form support-form'
-          onSubmit={handleSubmit(onSubmitForm)}
-        >
-          <div className='inputs-flex'>
-            <div className='input-block width-33'>
-              <input
-                type='text'
-                {...register('name', {
-                  maxLength: {
-                    value: 32,
-                    message: `*${setString(lang.formErrLongName)}`,
-                  },
-                })}
-              />
-              <div className='input-placeholder'>
-                {setString(lang.inputName)}
-              </div>
-              <p className='inpt-err-msg'>
-                {errors.name && errors.name.message}
-              </p>
-            </div>
-            <div className='input-block width-33'>
-              <input
-                type='tel'
-                {...register('phone', {
-                  required: `*${setString(lang.formErrEmptyPhone)}`,
-                  minLength: {
-                    value: 5,
-                    message: `*${setString(lang.formErrShortPhone)}`,
-                  },
-                })}
-              />
-              <div className='input-placeholder'>
-                {setString(lang.inputPhone)}
-              </div>
-              <p className='inpt-err-msg'>
-                {errors.phone && errors.phone.message}
-              </p>
-            </div>
-            <div className='input-block width-33'>
-              <button
-                type='submit'
-                className={`button white-button ${
-                  errors.name || errors.phone ? 'btn-disabled' : ''
-                }`}
-                disabled={errors.name || errors.phone ? true : false}
-              >
-                {setString(lang.inputSubmit)}
-              </button>
-            </div>
-          </div>
-          <div className='personal-datas'>
-            {setString(lang.privacyPolicyFirst)}{' '}
-            {/* <a href=''>{setString(lang.privacyPolicySecond)}</a> */}
-            {setString(lang.privacyPolicySecond)}
-          </div>
-        </form>
-        <button className='mfp-close' type='button' onClick={closePopUp}>
-          <img src='/assets/images/close.svg' alt='' />
-        </button>
-      </div>
-    </div>
-  )
+	return (
+		<div id='teachersModal' className='popup-modal mfp-hide mfp-with-anim'>
+			<div className='popup-content-origin red-bg'>
+				<h2>{title}</h2>
+				<div className='desc'>
+					{!programTitle && disc}{' '}
+					{programTitle &&
+						`Оставьте заявку и получите консультацию по программе ${programTitle}, узнайте возможные варианты скидок и требования к поступлению`}
+				</div>
+				<form
+					method='post'
+					className='simple-form support-form'
+					onSubmit={handleSubmit(onSubmitForm)}>
+					<div className='inputs-flex'>
+						<div className='input-block width-33'>
+							<input
+								type='text'
+								{...register('name', {
+									maxLength: {
+										value: 32,
+										message: `*${setString(lang.formErrLongName)}`,
+									},
+								})}
+							/>
+							<div className='input-placeholder'>
+								{setString(lang.inputName)}
+							</div>
+							<p className='inpt-err-msg'>
+								{errors.name && errors.name.message}
+							</p>
+						</div>
+						<div className='input-block width-33'>
+							<input
+								type='tel'
+								{...register('phone', {
+									required: `*${setString(lang.formErrEmptyPhone)}`,
+									minLength: {
+										value: 5,
+										message: `*${setString(lang.formErrShortPhone)}`,
+									},
+								})}
+							/>
+							<div className='input-placeholder'>
+								{setString(lang.inputPhone)}
+							</div>
+							<p className='inpt-err-msg'>
+								{errors.phone && errors.phone.message}
+							</p>
+						</div>
+						<div className='input-block width-33'>
+							<button
+								type='submit'
+								className={`button white-button ${
+									errors.name || errors.phone ? 'btn-disabled' : ''
+								}`}
+								disabled={errors.name || errors.phone ? true : false}>
+								{setString(lang.inputSubmit)}
+							</button>
+						</div>
+					</div>
+					<div className='personal-datas'>
+						{setString(lang.privacyPolicyFirst)}{' '}
+						{/* <a href=''>{setString(lang.privacyPolicySecond)}</a> */}
+						{setString(lang.privacyPolicySecond)}
+					</div>
+				</form>
+				<button className='mfp-close' type='button' onClick={closePopUp}>
+					<img src='/assets/images/close.svg' alt='' />
+				</button>
+			</div>
+		</div>
+	)
 }
 
 export default Form
--- components/sections/ContactUs.tsx
+++ components/sections/ContactUs.tsx
@@ -11,119 +11,122 @@ import { useEffect } from 'react'
 import loadJs from 'loadjs'
 
 type FormValues = {
-  name: string
-  phone: string
+	name: string
+	phone: string
 }
 
 const ContactUs = ({
-  programTitle = null,
-  programId = null,
-  title = setString(lang.helpToChooseTitle),
-  disc = setString(lang.helpToChooseDics),
+	programTitle = null,
+	programId = null,
+	title = setString(lang.helpToChooseTitle),
+	disc = setString(lang.helpToChooseDics),
 }) => {
-  const at = useAt()
+	const at = useAt()
 
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormValues>()
+	const {
+		register,
+		handleSubmit,
+		formState: { errors },
+	} = useForm<FormValues>()
 
-  useEffect(() => {
-    loadJs(['/assets/js/formPlaceholder.js'], {
-      async: false,
-    })
+	useEffect(() => {
+		loadJs(['/assets/js/formPlaceholder.js'], {
+			async: false,
+		})
 
-    const tagManagerArgs = {
-      dataLayer: {
-        event: 'generate_lead',
-        ecommerce: {
-          items: [
-            {
-              id: programId,
-              name: programTitle,
-              programFormat: at.online
-                ? 'online'
-                : at.blended
-                ? 'blended'
-                : null,
-              programType: at.mini
-                ? 'mini'
-                : at.professional
-                ? 'professional'
-                : at.industry
-                ? 'industry'
-                : null,
-            },
-          ],
-        },
-      },
-      dataLayerName: 'dataLayer',
-    }
-    TagManager.dataLayer(tagManagerArgs)
-  }, [])
+		const tagManagerArgs = {
+			dataLayer: {
+				event: 'generate_lead',
+				ecommerce: {
+					detail: {
+						actionField: {
+							id: programId,
+						},
+						products: [
+							{
+								id: programId,
+								name: programTitle,
+								programFormat: at.online
+									? 'online'
+									: at.blended
+									? 'blended'
+									: null,
+								programType: at.mini
+									? 'mini'
+									: at.professional
+									? 'professional'
+									: at.industry
+									? 'industry'
+									: null,
+							},
+						],
+					},
+				},
+			},
+			dataLayerName: 'dataLayer',
+		}
+		TagManager.dataLayer(tagManagerArgs)
+	}, [])
 
-  return (
-    <section className='support-section'>
-      <h2>{title}</h2>
-      <div className='text'>{disc}</div>
-      <form
-        method='post'
-        className='simple-form support-form embedded-form'
-        onSubmit={handleSubmit(onSubmitForm)}
-      >
-        <div className='inputs-flex'>
-          <div className='input-block width-33'>
-            <input
-              type='text'
-              {...register('name', {
-                maxLength: {
-                  value: 32,
-                  message: `*${setString(lang.formErrLongName)}`,
-                },
-              })}
-            />
-            <div className='input-placeholder'>{setString(lang.inputName)}</div>
-            <p className='inpt-err-msg'>{errors.name && errors.name.message}</p>
-          </div>
-          <div className='input-block width-33'>
-            <input
-              type='tel'
-              {...register('phone', {
-                required: `*${setString(lang.formErrEmptyPhone)}`,
-                minLength: {
-                  value: 5,
-                  message: `*${setString(lang.formErrShortPhone)}`,
-                },
-              })}
-            />
-            <div className='input-placeholder'>
-              {setString(lang.inputPhone)}
-            </div>
-            <p className='inpt-err-msg'>
-              {errors.phone && errors.phone.message}
-            </p>
-          </div>
-          <div className='input-block width-33'>
-            <button
-              type='submit'
-              className={`button white-button ${
-                errors.name || errors.phone ? 'btn-disabled' : ''
-              }`}
-              disabled={errors.name || errors.phone ? true : false}
-            >
-              {setString(lang.inputSubmit)}
-            </button>
-          </div>
-        </div>
-        <div className='personal-datas'>
-          {setString(lang.privacyPolicyFirst)}{' '}
-          {/* <a href=''>{setString(lang.privacyPolicySecond)}</a> */}
-          {setString(lang.privacyPolicySecond)}
-        </div>
-      </form>
-    </section>
-  )
+	return (
+		<section className='support-section'>
+			<h2>{title}</h2>
+			<div className='text'>{disc}</div>
+			<form
+				method='post'
+				className='simple-form support-form embedded-form'
+				onSubmit={handleSubmit(onSubmitForm)}>
+				<div className='inputs-flex'>
+					<div className='input-block width-33'>
+						<input
+							type='text'
+							{...register('name', {
+								maxLength: {
+									value: 32,
+									message: `*${setString(lang.formErrLongName)}`,
+								},
+							})}
+						/>
+						<div className='input-placeholder'>{setString(lang.inputName)}</div>
+						<p className='inpt-err-msg'>{errors.name && errors.name.message}</p>
+					</div>
+					<div className='input-block width-33'>
+						<input
+							type='tel'
+							{...register('phone', {
+								required: `*${setString(lang.formErrEmptyPhone)}`,
+								minLength: {
+									value: 5,
+									message: `*${setString(lang.formErrShortPhone)}`,
+								},
+							})}
+						/>
+						<div className='input-placeholder'>
+							{setString(lang.inputPhone)}
+						</div>
+						<p className='inpt-err-msg'>
+							{errors.phone && errors.phone.message}
+						</p>
+					</div>
+					<div className='input-block width-33'>
+						<button
+							type='submit'
+							className={`button white-button ${
+								errors.name || errors.phone ? 'btn-disabled' : ''
+							}`}
+							disabled={errors.name || errors.phone ? true : false}>
+							{setString(lang.inputSubmit)}
+						</button>
+					</div>
+				</div>
+				<div className='personal-datas'>
+					{setString(lang.privacyPolicyFirst)}{' '}
+					{/* <a href=''>{setString(lang.privacyPolicySecond)}</a> */}
+					{setString(lang.privacyPolicySecond)}
+				</div>
+			</form>
+		</section>
+	)
 }
 
 export default ContactUs
