78f21ce1ed2616a12849916075d58310693adcb9
Novailoveyou <novailoveyoux@gmail.com>
2021-05-11 19:49:05 +0300

    improved email submission

--- pages/api/contact.ts
+++ pages/api/contact.ts
@@ -79,278 +79,267 @@ export default async (req, res) => {
 
   const data = {
     id: uuidv4(),
-    name: name || '',
-    phone: phone || '',
-    email: '',
     date: now.format('DD-MM-YYYY'),
     time: now.format('HH:mm:ss'),
     utc: now.format('Z'),
+    name: name || '',
+    phone: phone || '',
+    email: '',
+    contactWay: '',
     root,
     path,
     ip,
-    cityEn: locationData.city.names.en,
-    cityRu: locationData.city.names.ru,
-    countryCode: locationData.country.code,
-    countryEn: locationData.country.names.en,
-    countryRu: locationData.country.names.ru,
-    continentCode: locationData.continent.code,
-    continentEn: locationData.continent.names.en,
-    continentRu: locationData.continent.names.ru,
-    accuracyRadius: locationData.coordinates.accuracyRadius,
-    latitude: locationData.coordinates.latitude,
-    longitude: locationData.coordinates.longitude,
-    timeZone: locationData.timeZone,
-    postalCode: locationData.postalCode
+    cityEn: locationData && locationData.city.names.en,
+    cityRu: locationData && locationData.city.names.ru,
+    countryCode: locationData && locationData.country.code,
+    countryEn: locationData && locationData.country.names.en,
+    countryRu: locationData && locationData.country.names.ru,
+    continentCode: locationData && locationData.continent.code,
+    continentEn: locationData && locationData.continent.names.en,
+    continentRu: locationData && locationData.continent.names.ru,
+    accuracyRadius: locationData && locationData.coordinates.accuracyRadius,
+    latitude: locationData && locationData.coordinates.latitude,
+    longitude: locationData && locationData.coordinates.longitude,
+    timeZone: locationData && locationData.timeZone,
+    postalCode: locationData && locationData.postalCode
   }
 
-  const html = /* html */ `
-  <!DOCTYPE html>
-  <html lang="ru">
-    <head>
-      <meta charset="UTF-8" />
-      <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  
-      <style>
-        .styled-table {
-          border-collapse: collapse;
-          margin: 25px 0;
-          font-size: 0.9em;
-          font-family: sans-serif;
-          min-width: 400px;
-          box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
-          counter-reset: tableCount;
-        }
-        .counterCell::before {
-          content: counter(tableCount);
-          counter-increment: tableCount;
-        }
-        .styled-table thead tr {
-          background-color: #ff3535;
-          color: #ffffff;
-          text-align: left;
-        }
-        .styled-table th,
-        .styled-table td {
-          padding: 12px 15px;
-        }
-        .styled-table tbody tr {
-          border-bottom: thin solid #dddddd;
-        }
-  
-        .styled-table tbody tr:nth-of-type(even),
-        .bgOnEven {
-          background-color: #f3f3f3;
-        }
-  
-        .styled-table tbody tr:last-of-type,
-        .bgBorderHighlight {
-          border-bottom: 2px solid #ff3535;
-        }
-        .styled-table tbody tr.active-row {
-          font-weight: bold;
-          color: #ff3535;
-        }
-      </style>
-      <title>Document</title>
-    </head>
-    <body>
-      <table class="styled-table">
-        <thead>
-          <tr>
-            <th>#</th>
-            <th>Ключ</th>
-            <th>Значение</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <td class="counterCell">1</td>
-            <td>ID</td>
-            <td>${data.id}</td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">2</td>
-            <td>Дата</td>
-            <td>${data.date}</td>
-          </tr>
-          <tr>
-            <td class="counterCell">3</td>
-            <td>Время</td>
-            <td>${data.time}</td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">3</td>
-            <td>UTC</td>
-            <td>${data.utc}</td>
-          </tr>
-          <tr>
-            <td class="counterCell">4</td>
-            <td>Имя</td>
-            <td>${data.name}</td>
-          </tr>
-          <tr class="active-row bgOnEven">
-            <td class="counterCell">5</td>
-            <td>Телефон</td>
-            <td>${data.phone}</td>
-          </tr>
-          <tr>
-            <td class="counterCell">6</td>
-            <td>Почта</td>
-            <td>${data.email}</td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">7</td>
-            <td>Способ связи</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td class="counterCell">8</td>
-            <td>Лид сайт</td>
-            <td>${data.root}</td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">9</td>
-            <td>Лид страница</td>
-            <td>${data.path}</td>
-          </tr>
-          <tr>
-            <td class="counterCell">10</td>
-            <td>IP</td>
-            <td>${data.ip}</td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">10</td>
-            <td>Город (en)</td>
-            <td>${data.cityEn}</td>
-          </tr>
-          <tr>
-            <td class="counterCell">10</td>
-            <td>Город (ru)</td>
-            <td>${data.cityRu}</td>
-          </tr>
+  const subject = 'Новая заявка с moscow.mba'
 
-          <tr class="bgOnEven">
-            <td class="counterCell">11</td>
-            <td>Код страны</td>
-            <td>${data.countryCode}</td>
-          </tr>
-          <tr>
-            <td class="counterCell">11</td>
-            <td>Страна (en)</td>
-            <td>${data.countryEn}</td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">11</td>
-            <td>Страна (ru)</td>
-            <td>${data.countryRu}</td>
-          </tr>
-          <tr>
-            <td class="counterCell">11</td>
-            <td>Континент код</td>
-            <td>${data.continentCode}</td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">11</td>
-            <td>Континент (en)</td>
-            <td>${data.continentEn}</td>
-          </tr>
-          <tr>
-            <td class="counterCell">11</td>
-            <td>Континент (ru)</td>
-            <td>${data.continentRu}</td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">11</td>
-            <td>Погрешность (м)</td>
-            <td>${data.accuracyRadius}</td>
-          </tr>
-          <tr>
-            <td class="counterCell">11</td>
-            <td>Широта</td>
-            <td>${data.latitude}</td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">11</td>
-            <td>Долгота</td>
-            <td>${data.longitude}</td>
-          </tr>
-          <tr>
-            <td class="counterCell">11</td>
-            <td>Часовой пояс</td>
-            <td>${data.timeZone}</td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">11</td>
-            <td>Зип код</td>
-            <td>${data.postalCode}</td>
-          </tr>
-          <tr>
-            <td class="counterCell">12</td>
-            <td>Направление</td>
-            <td></td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">13</td>
-            <td>Университет</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td class="counterCell">14</td>
-            <td>Вопрос</td>
-            <td></td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">15</td>
-            <td>Google Client ID</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td class="counterCell">16</td>
-            <td>Yandex Metrics ID</td>
-            <td></td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">17</td>
-            <td>Устройство пользователя</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td class="counterCell">18</td>
-            <td>Источник рекламы</td>
-            <td></td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">19</td>
-            <td>Тип трафика</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td class="counterCell">20</td>
-            <td>Название РК</td>
-            <td></td>
-          </tr>
-          <tr class="bgOnEven">
-            <td class="counterCell">21</td>
-            <td>Объявление</td>
-            <td></td>
-          </tr>
-          <tr>
-            <td class="counterCell">22</td>
-            <td>Ключевое слово</td>
-            <td></td>
-          </tr>
-          <tr class="bgOnEven bgBorderHighlight">
-            <td class="counterCell">23</td>
-            <td>Дубль</td>
-            <td></td>
-          </tr>
-        </tbody>
-      </table>
-    </body>
-  </html>
-  
-  `
+  const createEmailBody = () => {
+    const createTr = (item, idx) => {
+      const output = /* html */ `
+        <tr class="${idx % 2 === 0 ? 'bgOnEven' : ''} ${
+        item.trKey === 'Телефон' ? 'active-row' : ''
+      }">
+          <td class="counterCell">${idx}</td>
+          <td>${item.tdKey}</td>
+          <td>${item.tdVal}</td>
+        </tr>
+      `
+      return output
+    }
+
+    const tbodyTrs = [
+      {
+        tdKey: 'ID',
+        tdVal: data.id
+      },
+      {
+        tdKey: 'Дата',
+        tdVal: data.date
+      },
+      {
+        tdKey: 'Время',
+        tdVal: data.time
+      },
+      {
+        tdKey: 'UTC',
+        tdVal: data.utc
+      },
+      {
+        tdKey: 'Имя',
+        tdVal: data.name
+      },
+      {
+        tdKey: 'Телефон',
+        tdVal: data.phone
+      },
+      {
+        tdKey: 'Почта',
+        tdVal: data.email
+      },
+      {
+        tdKey: 'Способ связи',
+        tdVal: data.contactWay
+      },
+      {
+        tdKey: 'Лид сайт',
+        tdVal: data.root
+      },
+      {
+        tdKey: 'Лид страница',
+        tdVal: data.path
+      },
+      {
+        tdKey: 'IP',
+        tdVal: data.ip
+      },
+      {
+        tdKey: 'Город (en)',
+        tdVal: data.cityEn
+      },
+      {
+        tdKey: 'Город (ru)',
+        tdVal: data.cityRu
+      },
+      {
+        tdKey: 'Код страны',
+        tdVal: data.countryCode
+      },
+      {
+        tdKey: 'Страна (en)',
+        tdVal: data.countryEn
+      },
+      {
+        tdKey: 'Страна (ru)',
+        tdVal: data.countryRu
+      },
+      {
+        tdKey: 'Континент код',
+        tdVal: data.continentCode
+      },
+      {
+        tdKey: 'Континент (en)',
+        tdVal: data.continentEn
+      },
+      {
+        tdKey: 'Континент (ru)',
+        tdVal: data.continentRu
+      },
+      {
+        tdKey: 'Погрешность (м)',
+        tdVal: data.accuracyRadius
+      },
+      {
+        tdKey: 'Широта',
+        tdVal: data.latitude
+      },
+      {
+        tdKey: 'Долгота',
+        tdVal: data.longitude
+      },
+      {
+        tdKey: 'Часовой пояс',
+        tdVal: data.timeZone
+      },
+      {
+        tdKey: 'Зип код',
+        tdVal: data.postalCode
+      },
+      {
+        tdKey: 'Направление',
+        tdVal: ''
+      },
+      {
+        tdKey: 'Университет',
+        tdVal: ''
+      },
+      {
+        tdKey: 'Вопрос',
+        tdVal: ''
+      },
+      {
+        tdKey: 'Google Client ID',
+        tdVal: ''
+      },
+      {
+        tdKey: 'Yandex Metrics ID',
+        tdVal: ''
+      },
+      {
+        tdKey: 'Устройство пользователя',
+        tdVal: ''
+      },
+      {
+        tdKey: 'Источник рекламы',
+        tdVal: ''
+      },
+      {
+        tdKey: 'Тип трафика',
+        tdVal: ''
+      },
+      {
+        tdKey: 'Название РК',
+        tdVal: ''
+      },
+      {
+        tdKey: 'Объявление',
+        tdVal: ''
+      },
+      {
+        tdKey: 'Ключевое слово',
+        tdVal: ''
+      },
+      {
+        tdKey: 'Дубль',
+        tdVal: ''
+      }
+    ]
+
+    const output = /* html */ `
+      <!DOCTYPE html>
+      <html lang="ru">
+        <head>
+          <meta charset="UTF-8" />
+          <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+          <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      
+          <style>
+            .styled-table {
+              border-collapse: collapse;
+              margin: 25px 0;
+              font-size: 0.9em;
+              font-family: sans-serif;
+              min-width: 400px;
+              box-shadow: 0 0 20px rgba(0, 0, 0, 0.15);
+              counter-reset: tableCount;
+            }
+            .counterCell::before {
+              content: counter(tableCount);
+              counter-increment: tableCount;
+            }
+            .styled-table thead tr {
+              background-color: #ff3535;
+              color: #ffffff;
+              text-align: left;
+            }
+            .styled-table th,
+            .styled-table td {
+              padding: 12px 15px;
+            }
+            .styled-table tbody tr {
+              border-bottom: thin solid #dddddd;
+            }
+      
+            .styled-table tbody tr:nth-of-type(even),
+            .bgOnEven {
+              background-color: #f3f3f3;
+            }
+      
+            .styled-table tbody tr:last-of-type,
+            .bgBorderHighlight {
+              border-bottom: 2px solid #ff3535;
+            }
+            .styled-table tbody tr.active-row {
+              font-weight: bold;
+              color: #ff3535;
+            }
+          </style>
+          <title>${subject}</title>
+        </head>
+        <body>
+          <table class="styled-table">
+            <thead>
+              <tr>
+                <th>#</th>
+                <th>Ключ</th>
+                <th>Значение</th>
+              </tr>
+            </thead>
+            <tbody>
+              ${tbodyTrs.map(createTr).join('')}
+            </tbody>
+          </table>
+        </body>
+      </html>
+    `
+    return output
+  }
+
+  const html = createEmailBody()
+
+  console.log(html)
 
   // const testAccount = await nodemailer.createTestAccount()
 
@@ -377,7 +366,7 @@ export default async (req, res) => {
           ? 'nova@ipo.msk.ru, novailoveyou3@gmail.com'
           : 'marketing@rosucheba.ru'
       }`,
-      subject: 'Новая заявка с moscow.mba', // Subject line
+      subject, // Subject line
       text: `
       ${name}, \n
       ${phone}
