5aa76645f5b4926875a5deb51a185ab267d3f731
DrunkestManEvar <fullextentofsuperiority@mail.ru>
2021-06-06 15:10:28 +0300

    Extracted upToDateContent hugeStyles into a sass module

--- components/sections/UpToDateContent.tsx
+++ components/sections/UpToDateContent.tsx
@@ -1,21 +1,24 @@
+import stls from '@/styles/modules/UpToDateContent.module.sass'
 import setString from '@/components/hooks/setString'
 import lang from '@/data/translation/about'
 import Image from 'next/image'
 
 const UpToDateContent = () => {
   return (
-    <section className='actual-section no-line'>
+    <section className={[stls.actualSection, stls.noLine].join(' ')}>
       <div className='section-pl'>
         <h2>{setString(lang.uptodateTitle)}</h2>
       </div>
-      <div className='actual-content'>
-        <div className='actual-content-top'>
-          <div className='desc'>
+      <div className={stls.actualContent}>
+        <div className={stls.actualContentTop}>
+          <div className={stls.desc}>
             {setString(lang.uptodateDicsFirst)}{' '}
             <strong className='red'>{setString(lang.uptodateDicsRed)} </strong>
             {setString(lang.uptodateDicsSecond)}
           </div>
-          <div className='right-image'>
+          
+        </div>
+        <div className={stls.rightImage}>
             <Image
               src={'/assets/images/actual_pic_2.jpg'}
               alt='Оператор снимает конференцию'
@@ -23,8 +26,7 @@ const UpToDateContent = () => {
               height={635}
             />
           </div>
-        </div>
-        <div className='actual-bottom-image'>
+        <div className={stls.actualBottomImage}>
           <Image
             src={'/assets/images/actual_pic_1.jpg'}
             alt='Студенты обучаются на конференции'
@@ -34,22 +36,22 @@ const UpToDateContent = () => {
         </div>
       </div>
       <div className='section-pl'>
-        <ul className='actual-content-list'>
+        <ul className={stls.actualContentList}>
           <li>
-            <div className='number'>2021 {setString(lang.uptodateYear)}</div>
+            <div className={stls.number}>2021 {setString(lang.uptodateYear)}</div>
             <p>
               {setString(lang.uptodateBenefitOneDics)} 2021{' '}
               {setString(lang.uptodateYearV2)}
             </p>
           </li>
           <li>
-            <div className='number'>
+            <div className={stls.number}>
               53 {setString(lang.uptodateBenefitTwoTitle)}
             </div>
             <p>{setString(lang.uptodateBenefitTwoDics)}</p>
           </li>
           <li>
-            <div className='number'>
+            <div className={stls.number}>
               {setString(lang.uptodateBenefitThreeTitle)}
             </div>
             <p>{setString(lang.uptodateBenefitThreeDics)}</p>
