c8e80d721e853136ca259f672fa77d5752eb5a32
DrunkestManEvar <fullextentofsuperiority@mail.ru>
2021-06-28 13:29:29 +0300

    Fixed function responsible for getting declension number

--- components/sections/ProgramsNumber.tsx
+++ components/sections/ProgramsNumber.tsx
@@ -2,12 +2,12 @@ import setString from '@/components/hooks/setString'
 import langMenu from '@/data/translation/menu'
 
 const getStringDeclensionNumber = num => {
-  let value = Math.abs(num) % 100
-  let number = num % 10
+  const value = Math.abs(num) % 100
+  const number = num % 10
 
   if (value > 10 && value < 20) return 2
   if (number > 1 && number < 5) return 1
-  if (number == 1) return 0
+  if (number === 1) return 0
 
   return 2
 }
