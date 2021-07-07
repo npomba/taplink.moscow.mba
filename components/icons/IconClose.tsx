f982961c79db32b20a3f83662131988d811ff7c0
Novailoveyou <novailoveyou@gmail.com>
2021-07-07 17:55:03 +0300

    improved icons & wrapper them in container & added styles

--- components/icons/IconClose.tsx
+++ components/icons/IconClose.tsx
@@ -2,25 +2,23 @@ import stls from '@/styles/components/icons/IconClose.module.sass'
 
 const IconClose = ({ stroke = 'black' }) => {
   return (
-    <svg
-      width='14'
-      height='14'
-      viewBox='0 0 14 14'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'>
-      <path
-        d='M1 1L13 13'
-        stroke={stroke}
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-      <path
-        d='M13 1L1 13'
-        stroke={stroke}
-        strokeLinecap='round'
-        strokeLinejoin='round'
-      />
-    </svg>
+    <div className={stls.container}>
+      <svg viewBox='0 0 14 14' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <title>Закрыть</title>
+        <path
+          d='M1 1L13 13'
+          stroke={stroke}
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+        <path
+          d='M13 1L1 13'
+          stroke={stroke}
+          strokeLinecap='round'
+          strokeLinejoin='round'
+        />
+      </svg>
+    </div>
   )
 }
 
--- components/icons/IconCross.tsx
+++ components/icons/IconCross.tsx
@@ -2,16 +2,22 @@ import stls from '@/styles/components/icons/IconCross.module.sass'
 
 const IconCross = () => {
   return (
-    <svg
-      width='27'
-      height='27'
-      viewBox='0 0 27 27'
-      fill='none'
-      xmlns='http://www.w3.org/2000/svg'>
-      <circle opacity='0.1' cx='13.5' cy='13.5' r='13.5' fill='black' />
-      <path d='M18.837 8.16211L8.1626 18.8365' stroke='white' strokeWidth='2' />
-      <path d='M8.1626 8.16152L18.837 18.8359' stroke='white' strokeWidth='2' />
-    </svg>
+    <div className={stls.container}>
+      <svg viewBox='0 0 27 27' fill='none' xmlns='http://www.w3.org/2000/svg'>
+        <title>Крест</title>
+        <circle opacity='0.1' cx='13.5' cy='13.5' r='13.5' fill='black' />
+        <path
+          d='M18.837 8.16211L8.1626 18.8365'
+          stroke='white'
+          strokeWidth='2'
+        />
+        <path
+          d='M8.1626 8.16152L18.837 18.8359'
+          stroke='white'
+          strokeWidth='2'
+        />
+      </svg>
+    </div>
   )
 }
 
